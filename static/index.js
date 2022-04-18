9aa7f4715bf9900be16ef24f0764a2efbb054a54
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2022-04-17 20:00:19 -0700

    started styling

--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
-let map;
+let map, userMarker;
 
 function createMap() {
+	// create google maps object
 	map = new google.maps.Map(document.getElementById("map"), {
 		center: { lat: 44.565288, lng: -123.278921 },
 		zoom: 17,
@@ -11,4 +12,34 @@ function createMap() {
 		rotateControl: true,
 		fullscreenControl: false,
 	});
+
+	// try to get the current user position
+	getUserPosition();
+}
+
+function getUserPosition() {
+	// make sure geolocation is supported
+	if (!navigator.geolocation) {
+		return;
+	}
+
+	navigator.geolocation.getCurrentPosition(
+		(position) => {
+			const posObject = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude,
+			};
+			map.setCenter(posObject);
+			map.setZoom(17);
+
+			if (!userMarker) {
+				userMarker = new google.maps.Marker({ position: posObject, map });
+			} else {
+				userMarker.setPosition(posObject);
+			}
+		},
+		(e) => {
+			console.log("unable to get geolocation data: ", e);
+		}
+	);
 }
