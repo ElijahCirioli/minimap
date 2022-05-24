ee29d62c7b4b9b24747f2e03f04085d3ad2bfb0f
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2022-05-23 18:22:58 -0700

    yet more CORS

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.set("views", "./views");
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Credentials", true);
-	res.header("Access-Control-Allow-Methods", "GET,POST,UPDATE");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
