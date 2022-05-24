df8ef182b0f9f0822da31e355eaf9b3b67e18cec
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2022-05-23 18:27:45 -0700

    messing with CORS

--- server.js
+++ server.js
@@ -88,7 +88,6 @@ app.set("views", "./views");
 // setup HTTP CORS
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Credentials", true);
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.header(
 		"Access-Control-Allow-Headers",
